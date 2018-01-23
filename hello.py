ddbaa94ce9d1af46244e130342e35dd9cf21fbb7
Yaqi Zhang <zhang623@wisc.edu>
2018-01-23 12:02:14 -0600

    update notes and numpy

---
+++ hello.py
@@ -0,0 +1,12 @@
+"""
+written by Yaqi Zhang (zhang623@wisc.edu)
+University of Wisconsin-Madison
+
+"""
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    print("Hello World")
+
