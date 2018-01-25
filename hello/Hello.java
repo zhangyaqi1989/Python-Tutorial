8c22e2bb84798e6b84651e93c4d4cc7bee6d1546
Yaqi Zhang <zhang623@wisc.edu>
2018-01-25 00:04:35 -0600

    update

---
+++ hello/Hello.java
@@ -0,0 +1,15 @@
+/*
+ * written by Yaqi Zhang (zhang623@wisc.edu)
+ * University of Wisconsin-Madison
+ *
+ */
+
+public class Hello {
+
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World");
+  }
+}
+
+
---
+++ hello/hello.py
@@ -0,0 +1,5 @@
+"""
+written by Yaqi Zhang (zhang623@wisc.edu)
+University of Wisconsin-Madison
+"""
+print("Hello World")
