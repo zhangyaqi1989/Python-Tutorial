8a2a2fc69e0adee197f1ee927dfbddb844d54f63
Yaqi Zhang <zhang623@wisc.edu>
2018-02-20 18:44:15 -0600

    update

--- command-line-args/command_line_args.py
+++ command-line-args/command_line_args.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 written by Yaqi Zhang (zhang623@wisc.edu)
 University of Wisconsin-Madison
@@ -5,13 +6,15 @@ University of Wisconsin-Madison
 """
 import sys
 
-if __name__ == "__main__":
+def main():
     # sys.argv is a string list
     for i, arg in enumerate(sys.argv):
         print("argv[{0}] = {1}".format(i, arg))
-
     # cover string to int/float
     s_int = '100'
     print(int(s_int))
     s_float = '3.14'
     print(float(s_float))
+
+if __name__ == "__main__":
+    main()
---
+++ numpy/readfile/readfile.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Univertity of Wisconsin-Madison
+Yaqi Zhang
+"""
+import numpy as np
+# import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    M = np.loadtxt("input.dat")
+    print(M)
+
